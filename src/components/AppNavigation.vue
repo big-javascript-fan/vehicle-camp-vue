94f971f2fcee2c477f788463b088954138267072
javascripFan <wdeveloping9099@gmail.com>
2019-04-04 20:09:49 -0700

    add style to navigation

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -1,17 +1,29 @@
 <template>
     <v-toolbar app color="brown darken-4" dark>
-        <v-toolbar-side-icon></v-toolbar-side-icon>
+        <v-toolbar-side-icon class="hidden-md-and-up" @click="drawer = !drawer"></v-toolbar-side-icon>
+        <v-spacer class="hidden-md-and-up"></v-spacer>
         <v-toolbar-title>{{appTitle}}</v-toolbar-title>
-        <v-btn flat>Menu</v-btn>
-        <v-spacer></v-spacer>
-        <v-btn flat>SIGN IN</v-btn>
-        <v-btn color="brow lighten-3">JOIN</v-btn>
+        <v-btn flat class="hidden-md-and-down">Menu</v-btn>
+        <v-spacer class="hidden-md-and-down"></v-spacer>
+        <v-btn flat class="hidden-md-and-down">SIGN IN</v-btn>
+        <v-btn color="brow lighten-3" class="hidden-md-and-down">JOIN</v-btn>
     </v-toolbar>    
 </template>
 
 <script>
 export default {
-    name: 'AppNavigation'
+    name: 'AppNavigation',
+    data() {
+        return {
+            appTitle: 'Meal Prep',
+            drawer: false,
+            items: [
+                {title: 'Menu'},
+                {title: 'Sign In'},
+                {title: 'Join'}
+            ]
+        };
+    }
 };
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
     <div class="home">
         <img alt="Vue logo" src="../assets/logo.png" />
-        <HelloWorld msg="Welcome to Your Vue.js App" />
+        <AppNavigation appTitle="Meal Prep" />
     </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import AppNavigation from '@/components/AppNavigation.vue';
 
 export default {
     name: 'home',
     components: {
-        HelloWorld
+        AppNavigation
     }
 };
 </script>
