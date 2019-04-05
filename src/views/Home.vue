128ff6af9a9b8273d165cdb23caa80c8e1b15bbe
javascripFan <wdeveloping9099@gmail.com>
2019-04-04 20:50:37 -0700

    add details on homepage'

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,11 +7,13 @@
 <script>
 // @ is an alias to /src
 import HomeHero from '@/components/HomeHero';
+import HomeDetails from '@/components/HomeDetails';
 
 export default {
     name: 'home',
     components: {
-        HomeHero
+        HomeHero,
+        HomeDetails
     }
 };
 </script>
